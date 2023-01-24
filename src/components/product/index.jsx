9f70e7e8e6185fe0cd71567c7d3e5b27c9d1739e
Pere1J <pere1josep@gmail.com>
2023-01-24 11:39:41 +0100

    added product component style

--- src/components/product/index.jsx
+++ src/components/product/index.jsx
@@ -1,22 +1,29 @@
 import styles from "./product.module.css";
 import React from "react";
-import {
-  MdAddShoppingCart,
-  MdDeleteForever,
-  MdBorderColor,
-} from "react-icons/md";
+import { MdAddShoppingCart, MdDeleteForever, MdModeEdit } from "react-icons/md";
 
-export const Product = (props) => {
+export const Product = ({ product, buyById, editProductById, deleteById }) => {
   /*  {
-    id:'',
+    id:'', 
     productName:'',
     isBought: false,    
 };  */
   return (
-    <li key={props.product.id} className={styles.product}>
-      {props.product.productName}{" "}
-      <div>
-        <MdAddShoppingCart /> <MdBorderColor /> <MdDeleteForever />
+    <li key={product.id} className={styles.product}>
+      {product.productName}
+      <div className={styles.productButtonsContainer}>
+        <MdAddShoppingCart
+          className={`${styles.icon} ${styles.iconShopped}`}
+          onClick={() => buyById(product.id)}
+        />
+        <MdModeEdit
+          className={`${styles.icon} ${styles.iconEdit}`}
+          onClick={() => editProductById(product.id)}
+        />
+        <MdDeleteForever
+          className={`${styles.icon} ${styles.iconDelete}`}
+          onClick={() => deleteById(product.id)}
+        />
       </div>
     </li>
   );
