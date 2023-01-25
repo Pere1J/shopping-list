73fd5ca88e619895347dfc2990188d658b8488e5
jaume vidal <jaumevibu@gmail.com>
2023-01-25 17:36:06 +0100

    implemented loading&saving productlist into mockapi.io

--- src/components/productslist/index.jsx
+++ src/components/productslist/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 import { InputForm } from "../inputForm";
 import { Product } from "../product";
@@ -29,30 +29,31 @@ const dataBase = [
 export const ProductsList = () => {
   const [productsList, setProductsList] = useState(dataBase); //State
   const buyById = (id) => {
-    const filteredProduct = productsList.filter((product) => id == product.id);
+    const filteredProduct = productsList.filter((product) => id === product.id);
     if (filteredProduct.length > 1) {
       throw new Error("¡Parámetros repetidos!");
     }
-    if (filteredProduct.length == 1) {
+    if (filteredProduct.length === 1) {
       filteredProduct[0].isBought = !filteredProduct[0].isBought;
       setProductsList([...productsList]);
     }
   };
   const deleteById = (id) => {
-    const filteredProduct = productsList.filter((product) => id == product.id);
+    const filteredProduct = productsList.filter((product) => id === product.id);
     if (filteredProduct.length > 1) {
       throw new Error("¡Parámetros repetidos!");
     }
-    if (filteredProduct.length == 1) {
-      setProductsList(productsList.filter((product) => id != product.id));
+    if (filteredProduct.length === 1) {
+      delFromApi(filteredProduct[0]);
+      setProductsList(productsList.filter((product) => id !== product.id));
     }
   };
   const editById = (id) => {
-    const filteredProduct = productsList.filter((product) => id == product.id);
+    const filteredProduct = productsList.filter((product) => id === product.id);
     if (filteredProduct.length > 1) {
       throw new Error("¡Parámetros repetidos!");
     }
-    if (filteredProduct.length == 1) {
+    if (filteredProduct.length === 1) {
       //TODO resolver como pasar el producto al input
     }
   };
@@ -63,8 +64,59 @@ export const ProductsList = () => {
       productName: givenProductName,
       isBought: false,
     };
+    postToApi(newProduct);
     setProductsList([...productsList, newProduct]);
   };
+
+  const delFromApi = (product) => {
+    const url =
+      "https://63d0dab1d5f0fa7fbdbebaac.mockapi.io/products/" + product.id;
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    fetch(url, options)
+      .then((res) => {
+        if (res.ok) {
+          return Promise.resolve("Product deleted.");
+        } else {
+          return Promise.reject("An error occurred.");
+        }
+      })
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const postToApi = (product) => {
+    const url = "https://63d0dab1d5f0fa7fbdbebaac.mockapi.io/products";
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(product),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
+
+  useEffect(() => {
+    fetch("https://63d0dab1d5f0fa7fbdbebaac.mockapi.io/products")
+      .then((response) => response.json())
+      .then((data) => setProductsList(data))
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div>
       <InputForm addProduct={addProduct} />
